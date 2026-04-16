c1259c057bdd6359213b937953b0412da248c974
thiago <engs-thiagoireno@camporeal.edu.br>
2026-04-15 22:18:37 -0300

    Criando as colocacoes utilizando o if

--- assets/app.js
+++ assets/app.js
@@ -22,4 +22,7 @@ function adicionarNovoCartao() {
     let pergunta = prompt("Digite a sua pergunta:")
     let resposta = prompt("Digite a sua resposta:")
 
+    if (categotia && pergunta && resposta) {
+        criar_cartao(categoria, pergunta, resposta)
+    }
 }
